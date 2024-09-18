388f01a47e379b14686ed24c3e71007f21c83d5f
Albina Anistratenko <albuska2013@gmail.com>
2024-09-18 17:47:29 +0300

    change next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+// const nextConfig = {};
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
+
+module.exports = {
+  images: {
+    domains: ["i.ytimg.com", "lasertag.net"],
+    deviceSizes: [
+      360, 400, 411, 450, 500, 576, 640, 750, 828, 1080, 1200, 1920,
+    ],
+  },
+};
