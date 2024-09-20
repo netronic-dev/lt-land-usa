6252e10bc4dd0a68f01b9881530c5ec7df498b01
Albina Anistratenko <albuska2013@gmail.com>
2024-09-20 15:17:48 +0300

    change env

--- src/components/PageLayout/PageLayout.tsx
+++ src/components/PageLayout/PageLayout.tsx
@@ -56,6 +56,11 @@ const PageLayout = (props: any) => {
     }
   };
 
+  console.log(
+    process.env.NEXT_PUBLIC_ASSET_PREFIX,
+    "process.env.NEXT_PUBLIC_ASSET_PREFIX"
+  );
+
   useEffect(() => {
     sendEventToConversionApi(window.location.href, "PageView");
 
