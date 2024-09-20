5853c2ff77fd695e99b8d876949580c51992f316
Albina Anistratenko <albuska2013@gmail.com>
2024-09-20 12:15:18 +0300

    add condition for publicRuntimeConfig

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
+const isProd = process.env.NODE_ENV === "production";
+
 module.exports = {
   assetPrefix: process.env.ASSET_PREFIX || "",
+  publicRuntimeConfig: {
+    assetPrefix: isProd ? "/version-b" : "",
+  },
   images: {
     domains: ["i.ytimg.com", "lasertag.net"],
     deviceSizes: [
