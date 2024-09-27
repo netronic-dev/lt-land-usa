b98a65a1f735bcc5cb2beb81c27be6a542452230
Albina Anistratenko <albuska2013@gmail.com>
2024-09-27 10:20:28 +0300

    add cssnano for optimizate css

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,21 @@
+// /** @type {import('postcss-load-config').Config} */
+// const config = {
+//   plugins: {
+//     tailwindcss: {},
+//   },
+// };
+
+// module.exports = config;
+
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
     tailwindcss: {},
+    "postcss-flexbugs-fixes": {},
+    "postcss-preset-env": {},
+    cssnano: {
+      preset: "default",
+    },
   },
 };
 
